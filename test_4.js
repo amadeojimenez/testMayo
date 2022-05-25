16dc07a48ef664632f2c0ea75900ba76fb47edb7
amadeojimenez <55413365+amadeojimenez@users.noreply.github.com>
2022-05-25 09:18:20 +0200

    lala

--- test_4.js
+++
@@ -1,5 +0,0 @@
-//----------------------------------------------- 4
-//Escribe una api-rest con express que tenga dos rutas, una para subir un número y otra para obtener dicho número
-//Añadir un middleware que haga console.log() de la ruta total a) con querystrings b) sin querystrings
-//Crear y añadir un middleware que de manera aleatoria genere un error
-//Handlear el error con una function (err,req,res,next)
